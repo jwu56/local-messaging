396be5191e02765f3087040b75a22ce7177a6f39
KLZX121 <kenneth121lzx@gmail.com>
2021-02-16 21:56:29 +1000

    updated for packaging

--- main.js
+++ main.js
@@ -10,15 +10,12 @@ function boot(){
         minimizable: false,
         maximizable: false,
         show: false,
-        icon: path.join(__dirname, './build/icon.png'),
         webPreferences: {
             nodeIntegration: true
         }
     });
-    win.loadFile(path.join(__dirname,'./renderer/index.html'));
+    win.loadFile('./renderer/index.html');
     win.webContents.on('did-finish-load', () => win.show());
-
-    win.webContents.openDevTools();
 
     win.on('close', function (event) {
         if (!app.isQuitting){
@@ -28,17 +25,25 @@ function boot(){
         return false;
     });
 
-    const contextMenu = Menu.buildFromTemplate([
-        { 
-            label: 'Quit', 
-            click:  function(){
-                app.isQuitting = true;
-                app.quit();
-            } 
-        }
-    ]);
+    const contextMenu = Menu.buildFromTemplate(
+        [
+            { 
+                label: 'Quit', 
+                click:  function(){
+                    app.isQuitting = true;
+                    app.quit();
+                } 
+            },
+            {
+                label: 'Show Devtools',
+                click: () => {
+                    win.webContents.openDevTools();
+                }
+            }
+        ]
+    );
 
-    tray = new Tray(path.join(__dirname,'./build/icon.png'));
+    tray = new Tray(path.join(app.getAppPath(),'./imgs/tray.ico'));
     tray.setToolTip('Right click for options');
     tray.setContextMenu(contextMenu);
     tray.on('click', () => win.show())
